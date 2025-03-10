84ef4368aa37f834532eab167ba3834d46241777
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-10 14:33:03 +0800

    实现Jwt令牌的生成，同时修改Jwt配置文件中秘钥（新版jjwt设置密钥长度必须大于256位）

--- online-study-backend/online-study-common/src/main/java/com/study/utils/JwtUtil.java
+++ online-study-backend/online-study-common/src/main/java/com/study/utils/JwtUtil.java
@@ -6,6 +6,9 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * jwt令牌工具类
@@ -27,5 +30,30 @@ public class JwtUtil {
                 .parseSignedClaims(token); // 解析JWT Token，返回Jws<Claims>对象
 
         return jws.getPayload(); // 从Jws<Claims>对象中提取Claims（载荷数据）
+    }
+
+    /**
+     * 生成jwt
+     * 使用Hs256算法, 私匙使用固定秘钥
+     *
+     * @param secretKey jwt秘钥
+     * @param ttlMillis jwt过期时间(毫秒)
+     * @param claims    设置的信息
+     * @return jwt令牌字符串
+     */
+    public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
+        // 生成符合 JJWT 规范的 HMAC-SHA 密钥
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        // 计算过期时间
+        long expMillis = System.currentTimeMillis() + ttlMillis;
+        Date exp = new Date(expMillis);
+
+        // 创建 JWT 令牌
+        return Jwts.builder()
+                .claims(claims)     // 设置自定义载荷
+                .expiration(exp)    // 设置过期时间
+                .signWith(key)      // 使用密钥进行签名
+                .compact();         // 生成 JWT 字符串
     }
 }
