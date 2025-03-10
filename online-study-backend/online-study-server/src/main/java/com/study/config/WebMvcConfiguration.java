794c4a10f91474ac3e5c2a705874c162131a01af
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-10 10:41:52 +0800

    注册并配置B端Jwt令牌拦截器的拦截范围

---
+++ online-study-backend/online-study-server/src/main/java/com/study/config/WebMvcConfiguration.java
@@ -0,0 +1,30 @@
+package com.study.config;
+
+
+import com.study.interceptor.JwtTokenAdminInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 配置类，注册Web层相关组件
+ */
+
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
+
+    /**
+     * 自定义拦截器*/
+    protected void addInterceptors(InterceptorRegistry registry) {
+        log.info("开始注册自定义拦截器");
+        registry.addInterceptor(jwtTokenAdminInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/user/login");
+    }
+}
