5b4d2e5a2e6addc9390b9a28f1ee3c0355bb5f59
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-10 19:11:09 +0800

    实现全局业务异常捕获、处理的功能

---
+++ online-study-backend/online-study-server/src/main/java/com/study/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.study.handler;
+
+import com.study.exception.BaseException;
+import com.study.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器，处理项目中抛出的业务异常
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获业务异常
+     *
+     * @param e BaseException 业务异常类
+     * @return Result响应对象类
+     */
+    @ExceptionHandler
+    public Result<String> exceptionHandler(BaseException e) {
+        log.error("异常信息：{}", e.getMessage());
+        return Result.error(e.getMessage());
+    }
+}
