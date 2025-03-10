a09b22af4fd24a3b3c6632be1e968896d997f433
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-10 16:00:31 +0800

    将序列化与反序列化的对象映射器类加入Spring MVC的消息转化器类对象中

--- online-study-backend/online-study-server/src/main/java/com/study/config/WebMvcConfiguration.java
+++ online-study-backend/online-study-server/src/main/java/com/study/config/WebMvcConfiguration.java
@@ -3,11 +3,16 @@ package com.study.config;
 
 import com.study.interceptor.JwtTokenAdminInterceptor;
 import com.study.interceptor.JwtTokenClientInterceptor;
+import com.study.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * 配置类，注册Web层相关组件
@@ -35,5 +40,20 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenClientInterceptor)
                 .addPathPatterns("/client/**")
                 .excludePathPatterns("/client/user/login");
+    }
+
+    /**
+     * 拓展Spring MVC的消息转化器
+     *
+     * @param converters 消息转化器类对象
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建一个消息转换器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        // 需要为消息转换器设置一个对象转换器，对象转换器可以将Java对象序列为Json数据
+        converter.setObjectMapper(new JacksonObjectMapper());
+        // 将自己的消息转化器加入容器中
+        converters.add(0, converter);
     }
 }
