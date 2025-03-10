667581236d89c2c3ac88afa0837d708baf9a9fa1
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-10 19:15:05 +0800

    创建信息提示常量类

---
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
@@ -0,0 +1,10 @@
+package com.study.constant;
+
+/**
+ * 信息提示常量类
+ */
+public class MessageConstant {
+
+    public static final String UNKNOWN_ERROR = "未知错误";
+    public static final String ALREADY_EXISTS = "已存在";
+}
