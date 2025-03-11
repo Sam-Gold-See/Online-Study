212ebef3e9931236c139588eca7ef7d66e3be2d4
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-11 10:57:20 +0800

    Id前置标识符常量类

---
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/IdConstant.java
@@ -0,0 +1,13 @@
+package com.study.constant;
+
+/**
+ * Id前置标识常量类
+ */
+public class IdConstant {
+
+    //管理员Id前置符
+    public static final String ADMIN_SIGNAL = "1";
+
+    //用户Id前置标识符
+    public static final String CLIENT_SIGNAL = "2";
+}
