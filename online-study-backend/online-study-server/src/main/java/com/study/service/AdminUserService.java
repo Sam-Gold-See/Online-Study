0207632f894851adcce2f2e4e1ea2794d357f190
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-11 19:02:31 +0800

    Service层实现获取用户登录的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/AdminUserService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/AdminUserService.java
@@ -1,6 +1,8 @@
 package com.study.service;
 
 import com.study.dto.AdminUserDTO;
+import com.study.dto.AdminUserLoginDTO;
+import com.study.vo.AdminUserLoginVO;
 
 public interface AdminUserService {
 
@@ -10,4 +12,12 @@ public interface AdminUserService {
      * @param adminUserDTO B端用户DTO对象
      */
     void add(AdminUserDTO adminUserDTO);
+
+    /**
+     * 用户登录
+     *
+     * @param adminUserLoginDTO B端用户登录DTO对象
+     * @return AdminUserLoginVO B端用户登录VO对象
+     */
+    AdminUserLoginVO login(AdminUserLoginDTO adminUserLoginDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/AdminUserServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/AdminUserServiceImpl.java
@@ -1,24 +1,39 @@
 package com.study.service.impl;
 
+import com.study.constant.AdminUserConstant;
 import com.study.constant.IdConstant;
+import com.study.constant.JwtClaimsConstant;
 import com.study.constant.MessageConstant;
 import com.study.context.BaseContext;
 import com.study.dto.AdminUserDTO;
+import com.study.dto.AdminUserLoginDTO;
 import com.study.entity.AdminUser;
+import com.study.exception.AccountNotFoundException;
 import com.study.exception.AdminUserLevelException;
+import com.study.exception.AdminUserStatusException;
+import com.study.exception.PasswordErrorException;
 import com.study.mapper.AdminUserMapper;
+import com.study.properties.JwtProperties;
 import com.study.service.AdminUserService;
 import com.study.utils.IdGeneratorUtil;
+import com.study.utils.JwtUtil;
+import com.study.vo.AdminUserLoginVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class AdminUserServiceImpl implements AdminUserService {
 
     @Autowired
     private AdminUserMapper adminUserMapper;
+    @Autowired
+    private JwtProperties jwtProperties;
 
     /**
      * 新增B端用户
@@ -41,5 +56,49 @@ public class AdminUserServiceImpl implements AdminUserService {
         adminUser.setId(Id);
 
         adminUserMapper.insert(adminUser);
+    }
+
+    /**
+     * 用户登录
+     *
+     * @param adminUserLoginDTO B端用户登录DTO对象
+     * @return AdminUserLoginVO B端用户登录VO对象
+     */
+    @Override
+    public AdminUserLoginVO login(AdminUserLoginDTO adminUserLoginDTO) {
+        String username = adminUserLoginDTO.getUsername();
+
+        // 根据用户登录账号查询用户库数据
+        AdminUser adminUserDB = adminUserMapper.getByUsername(username);
+        if (adminUserDB == null)
+            // 账号不存在
+            throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
+
+        String password = adminUserLoginDTO.getPassword();
+        // 密码加密成暗文，在数据库中密码以暗文形式存储
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        if (!password.equals(adminUserDB.getPassword()))
+            // 密码错误
+            throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
+
+        if (!Objects.equals(adminUserDB.getStatus(), AdminUserConstant.ENABLED))
+            // 账号被锁定无法登陆
+            throw new AdminUserStatusException(MessageConstant.ACCOUNT_LOCKED);
+
+        // 生成JWT令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.ADMIN_ID, adminUserDB.getId());
+        String token = JwtUtil.createJWT(
+                jwtProperties.getAdminSecretKey(),
+                jwtProperties.getAdminTtl(),
+                claims);
+
+        return AdminUserLoginVO.builder()
+                .id(adminUserDB.getId())
+                .name(adminUserDB.getName())
+                .username(username)
+                .token(token)
+                .build();
     }
 }
