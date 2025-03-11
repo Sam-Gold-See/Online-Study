fdb3ed9576e27dff7e20135eebc6c2f21e2d5518
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-11 20:10:19 +0800

    实现C端用户实体类

---
+++ online-study-backend/online-study-pojo/src/main/java/com/study/entity/ClientUser.java
@@ -0,0 +1,47 @@
+package com.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientUser implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // 时间戳唯一id
+    private Long id;
+
+    // 用户名
+    private String name;
+
+    // 用户手机
+    private String phone;
+
+    // 用户登录密码
+    private String password;
+
+    // 用户性别（M：男，女：F）
+    private Character gender;
+
+    // 使用权限（1=正常，0=禁用）
+    private Integer status;
+
+    // 头像资源链接
+    private String avatar;
+
+    // 创建时间
+    private LocalDateTime createTime;
+
+    // 个人信息更新时间
+    private LocalDateTime updateTime;
+}
