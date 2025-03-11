8ad8968c22523316c7c0d6a898e2d01c6373db25
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-11 19:05:55 +0800

    完善管理员用户相关的常量类声明

---
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/AdminUserConstant.java
@@ -0,0 +1,16 @@
+package com.study.constant;
+
+public class AdminUserConstant {
+
+    // 账号状态正常可登录
+    public static final Integer ENABLED = 1;
+
+    // 账号状态异常不可登录
+    public static final Integer DISABLED = 0;
+
+    // 账号有权限修改信息
+    public static final Integer PERMISSION = 1;
+
+    // 账号无权限修改信息
+    public static final Integer PERMISSION_DENIED = 0;
+}
