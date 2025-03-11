5cbc69a5d53d07a31c728b25eef32dc8337a8f9c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-11 19:54:01 +0800

    更新数据库初始化SQL语句，新建C端用户表以及插入初始数据

--- online-study-backend/online-study-server/src/main/resources/InitSQL.sql
+++ online-study-backend/online-study-server/src/main/resources/InitSQL.sql
@@ -22,10 +22,24 @@ CREATE TABLE admin_user
     create_user BIGINT COMMENT '创建用户ID',
     update_user BIGINT COMMENT '更新用户ID',
     `level`     TINYINT DEFAULT 0 COMMENT '是否有权限管理用户（1=有权限, 0=无权限）'
-) COMMENT='管理员用户表';
+) COMMENT='管理端用户表';
 
 INSERT INTO admin_user (id, name, username, password, phone, gender, status, create_time, update_time, create_user,
                         update_user, level)
 VALUES (11741666652149400, 'Admin', 'admin', '123456', '15889925075', 'F', 1,
         '2025-03-11 12:15:21', '2025-03-11 12:15:21', 11741666213042300, 11741666213042300, 1);
 
+CREATE TABLE client_user
+(
+    id          BIGINT      NOT NULL PRIMARY KEY COMMENT '时间戳唯一id',
+    `name`      VARCHAR(32) COMMENT '用户名',
+    phone       VARCHAR(15) UNIQUE COMMENT '用户手机，同为登录账号',
+    `password`  VARCHAR(64) NOT NULL COMMENT '用户登录密码',
+    gender      CHAR(1) COMMENT '用户性别（M: 男，F: 女）',
+    `status`    TINYINT DEFAULT 1 COMMENT '使用权限（1=可登录, 0=禁用）',
+    avatar      String COMMENT '头像资源链接',
+    create_time DATETIME COMMENT '创建时间'
+) COMMENT='客户端用户表'
+
+INSERT INTO client_user (id, name, phone, password, gender, status, avatar, create_time)
+VALUES (2174169369495300,'SamGoldSee', '15889925075', 'M', 1, '', '2025-03-11 19:48:14');
