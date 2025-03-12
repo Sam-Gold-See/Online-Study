e9eb45b2bc47d21c0afa28ecdd5a6a397b249be3
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-12 09:36:58 +0800

    Mapper层实现插入新增数据的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/ClientUserMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/ClientUserMapper.java
@@ -1,7 +1,23 @@
 package com.study.mapper;
 
+import com.study.annotation.AutoFill;
+import com.study.entity.ClientUser;
+import com.study.enumeration.OperationType;
+import com.study.enumeration.TerminalType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ClientUserMapper {
+
+    /**
+     * 插入数据
+     *
+     * @param clientUser 客户端用户实体类
+     */
+    @AutoFill(operation = OperationType.INSERT, terminal = TerminalType.CLIENT)
+    @Insert("INSERT INTO client_user " +
+            "(id, name, phone, password, gender, avatar, create_time, update_time) " +
+            "VALUES(#{id}, #{name}, #{phone}, #{password}, #{gender}, #{avatar}, #{createTime}, #{updateTime})")
+    void insert(ClientUser clientUser);
 }
