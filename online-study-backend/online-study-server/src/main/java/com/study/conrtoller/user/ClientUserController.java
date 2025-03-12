55d9aec5ae52286af34fa24fb089fa710c559f52
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-12 09:37:41 +0800

    Controller层实现获取新增用户的请求

--- online-study-backend/online-study-server/src/main/java/com/study/conrtoller/user/ClientUserController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/conrtoller/user/ClientUserController.java
@@ -1,8 +1,12 @@
 package com.study.conrtoller.user;
 
+import com.study.dto.ClientUserDTO;
+import com.study.result.Result;
 import com.study.service.ClientUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,16 @@ public class ClientUserController {
 
     @Autowired
     ClientUserService clientUserService;
+
+    /**
+     * 新增C端用户
+     *
+     * @param clientUserDTO C端用户DTO对象
+     * @return Result类响应对象
+     */
+    @PostMapping("/add")
+    public Result<String> add(@RequestBody ClientUserDTO clientUserDTO) {
+        clientUserService.add(clientUserDTO);
+        return Result.success();
+    }
 }
