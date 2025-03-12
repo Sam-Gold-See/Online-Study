96a3e61ca70162f308654d8c05a5c559129e7636
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-12 10:34:13 +0800

    Mapper层实现根据用户手机号/账号查询用户信息的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/ClientUserMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/ClientUserMapper.java
@@ -6,6 +6,7 @@ import com.study.enumeration.OperationType;
 import com.study.enumeration.TerminalType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ClientUserMapper {
@@ -20,4 +21,13 @@ public interface ClientUserMapper {
             "(id, name, phone, password, gender, avatar, create_time, update_time) " +
             "VALUES(#{id}, #{name}, #{phone}, #{password}, #{gender}, #{avatar}, #{createTime}, #{updateTime})")
     void insert(ClientUser clientUser);
+
+    /**
+     * 根据手机号查询用户数据
+     *
+     * @param phone 客户端用户手机号、登录账号
+     * @return ClientUser客户端用户实体类
+     */
+    @Select("SELECT * FROM client_user WHERE phone = #{phone}")
+    ClientUser getByPhone(String phone);
 }
