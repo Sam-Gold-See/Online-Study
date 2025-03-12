18d0f1ab5b4c1545a6914e75d27446dd4e2634d9
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-12 09:37:15 +0800

    Service层实现新增C端用户的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/ClientUserService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/ClientUserService.java
@@ -1,4 +1,13 @@
 package com.study.service;
 
+import com.study.dto.ClientUserDTO;
+
 public interface ClientUserService {
+
+    /**
+     * 新增C端用户
+     *
+     * @param clientUserDTO C端用户DTO对象
+     */
+    void add(ClientUserDTO clientUserDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/ClientUserServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/ClientUserServiceImpl.java
@@ -1,13 +1,36 @@
 package com.study.service.impl;
 
+import com.study.constant.IdConstant;
+import com.study.dto.ClientUserDTO;
+import com.study.entity.ClientUser;
 import com.study.mapper.ClientUserMapper;
 import com.study.service.ClientUserService;
+import com.study.utils.IdGeneratorUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class ClientUserServiceImpl implements ClientUserService {
 
     @Autowired
     ClientUserMapper clientUserMapper;
+
+    /**
+     * 新增C端用户
+     *
+     * @param clientUserDTO C端用户DTO对象
+     */
+    @Override
+    public void add(ClientUserDTO clientUserDTO) {
+         ClientUser clientUser = new ClientUser();
+         BeanUtils.copyProperties(clientUserDTO, clientUser);
+         clientUser.setPassword(DigestUtils.md5DigestAsHex(clientUserDTO.getPassword().getBytes()));
+
+         Long id = IdGeneratorUtil.generateId(IdConstant.CLIENT_SIGNAL);
+         clientUser.setId(id);
+
+         clientUserMapper.insert(clientUser);
+    }
 }
