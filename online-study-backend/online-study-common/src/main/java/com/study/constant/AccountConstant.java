ac92327ac074b3fd862fab181575038b746b038c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-12 16:16:20 +0800

    完善账户相关常量类设置

--- online-study-backend/online-study-common/src/main/java/com/study/constant/AccountConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/AccountConstant.java
@@ -13,4 +13,13 @@ public class AccountConstant {
 
     // 账号无权限修改信息
     public static final Integer PERMISSION_DENIED = 0;
+
+    // 账号操作的验证码起始位置
+    public static final Integer VERIFICATION_CODE_START = 0;
+
+    // 账号操作的验证码长度
+    public static final Integer VERIFICATION_CODE_LENGTH = 6;
+
+    // 验证码有效时长
+    public static final Integer VERIFICATION_CODE_TTL = 1;
 }
