a95394b146c93249ae7cb9236d886ae6008e10be
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-13 10:48:40 +0800

    Controller层实现获取启用、禁用B端用户登录权限请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/conrtoller/admin/AdminUserController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/conrtoller/admin/AdminUserController.java
@@ -7,10 +7,7 @@ import com.study.service.AdminUserService;
 import com.study.vo.AdminUserLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/user")
@@ -42,5 +39,17 @@ public class AdminUserController {
     public Result<AdminUserLoginVO> login(@RequestBody AdminUserLoginDTO adminUserLoginDTO) {
         AdminUserLoginVO adminUserLoginVO = adminUserService.login(adminUserLoginDTO);
         return Result.success(adminUserLoginVO);
+    }
+
+    /**
+     * 启用、禁用B端用户登录权限
+     *
+     * @param id 用户id
+     * @return Result类响应对象
+     */
+    @GetMapping("/editStatus")
+    public Result<String> editStatus(Long id, Integer status) {
+        adminUserService.editStatus(id, status);
+        return Result.success();
     }
 }
