cb465b7de648598fdbd9c51d9525e40af6fc8567
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-15 11:00:00 +0800

    优化初始化数据库SQL文件

--- online-study-backend/online-study-server/src/main/resources/InitSQL.sql
+++ online-study-backend/online-study-server/src/main/resources/InitSQL.sql
@@ -16,12 +16,12 @@ CREATE TABLE admin_user
     `password`  VARCHAR(64) NOT NULL COMMENT '用户登录密码',
     phone       VARCHAR(15) UNIQUE COMMENT '用户手机',
     gender      CHAR(1) COMMENT '用户性别（M: 男，F: 女）',
-    `status`    TINYINT DEFAULT 1 COMMENT '使用权限（1=可登录, 0=禁用）',
+    `status`    TINYINT DEFAULT 1 COMMENT '登录权限（1=可登录, 0=禁用）',
     create_time DATETIME COMMENT '创建时间',
     update_time DATETIME COMMENT '更新时间',
     create_user BIGINT COMMENT '创建用户ID',
     update_user BIGINT COMMENT '更新用户ID',
-    `level`     TINYINT DEFAULT 0 COMMENT '是否有权限管理用户（1=有权限, 0=无权限）'
+    `level`     TINYINT DEFAULT 0 COMMENT '是否有权限管理B端用户（1=有权限, 0=无权限）'
 ) COMMENT='管理端用户表';
 
 INSERT INTO admin_user (id, name, username, password, phone, gender, status, create_time, update_time, create_user,
