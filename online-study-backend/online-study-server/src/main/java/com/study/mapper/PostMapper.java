7f2bb93a9cdd3319bbcfa621607fa12bcd16398d
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-18 10:21:16 +0800

    Mapper层实现新增帖子的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/PostMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/PostMapper.java
@@ -1,8 +1,22 @@
 package com.study.mapper;
 
+import com.study.annotation.AutoFill;
+import com.study.entity.Post;
+import com.study.enumeration.OperationType;
+import com.study.enumeration.TerminalType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PostMapper {
 
+    /**
+     * 新增帖子
+     *
+     * @param post 帖子实体类对象
+     */
+    @AutoFill(operation = OperationType.INSERT, terminal = TerminalType.CLIENT)
+    @Insert("INSERT INTO post (user_id, title, content, create_time, update_time, category_id) " +
+            "VALUES (#{userId}, #{title}, #{content}, #{createTime}, #{updateTime}, #{categoryId})")
+    void insert(Post post);
 }
