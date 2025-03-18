f1671dff283da8a54fdec4edf429e9e6af4a507e
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-18 10:20:52 +0800

    Controller层实现获取新增帖子请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/ClientPostController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/ClientPostController.java
@@ -1,10 +1,12 @@
 package com.study.controller.client;
 
+import com.study.dto.post.PostInsertDTO;
 import com.study.result.Result;
 import com.study.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +17,16 @@ public class ClientPostController {
 
     @Autowired
     private PostService postService;
+
+    /**
+     * 新增帖子
+     *
+     * @param postInsertDTO 帖子新增DTO对象
+     * @return Result类响应对象
+     */
+    @PostMapping("/insert")
+    public Result<String> insert(@RequestBody PostInsertDTO postInsertDTO) {
+        postService.insert(postInsertDTO);
+        return Result.success();
+    }
 }
