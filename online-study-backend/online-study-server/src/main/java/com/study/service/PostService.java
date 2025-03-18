6d894b07e8a1175aa817664ed5a9b72ce48dbf80
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-18 14:02:33 +0800

    Service层实现查询帖子信息的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/PostService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/PostService.java
@@ -1,13 +1,22 @@
 package com.study.service;
 
-import com.study.dto.post.PostInsertDTO;
+import com.study.dto.post.PostDTO;
+import com.study.vo.PostVO;
 
 public interface PostService {
 
     /**
      * 新增帖子
      *
-     * @param postInsertDTO 帖子新增DTO对象
+     * @param postDTO 帖子DTO对象
      */
-    void insert(PostInsertDTO postInsertDTO);
+    void insert(PostDTO postDTO);
+
+    /**
+     * 获取帖子信息
+     *
+     * @param id 帖子Id
+     * @return PostVO帖子VO对象
+     */
+    PostVO get(Long id);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/PostServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/PostServiceImpl.java
@@ -1,10 +1,13 @@
 package com.study.service.impl;
 
+import com.study.constant.MessageConstant;
 import com.study.context.BaseContext;
-import com.study.dto.post.PostInsertDTO;
+import com.study.dto.post.PostDTO;
 import com.study.entity.Post;
+import com.study.exception.PostNotFoundException;
 import com.study.mapper.PostMapper;
 import com.study.service.PostService;
+import com.study.vo.PostVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,16 +21,35 @@ public class PostServiceImpl implements PostService {
     /**
      * 新增帖子
      *
-     * @param postInsertDTO 帖子新增DTO对象
+     * @param postDTO 帖子新增对象
      */
     @Override
-    public void insert(PostInsertDTO postInsertDTO) {
+    public void insert(PostDTO postDTO) {
         Post post = new Post();
-        BeanUtils.copyProperties(postInsertDTO, post);
+        BeanUtils.copyProperties(postDTO, post);
 
         Long userId = BaseContext.getCurrentId();
         post.setUserId(userId);
 
         postMapper.insert(post);
+    }
+
+    /**
+     * 获取帖子信息
+     *
+     * @param id 帖子Id
+     * @return PostVO帖子VO对象
+     */
+    @Override
+    public PostVO get(Long id) {
+        Post post = postMapper.getById(id);
+
+        if(post == null)
+            throw new PostNotFoundException(MessageConstant.POST_NOT_FOUND);
+
+        PostVO postVO = new PostVO();
+        BeanUtils.copyProperties(post, postVO);
+
+        return postVO;
     }
 }
