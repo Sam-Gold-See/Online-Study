8840c0b5e5b9fb7bb25974c49c39bbef7627d3a4
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 13:27:37 +0800

    Controller层实现获取 C端用户登录 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/UserController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/UserController.java
@@ -1,8 +1,10 @@
 package com.study.controller.client;
 
+import com.study.dto.ClientUserLoginDTO;
 import com.study.dto.ClientUserRegistDTO;
 import com.study.result.Result;
 import com.study.service.ClientUserService;
+import com.study.vo.ClientUserLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,5 +29,16 @@ public class UserController {
     public Result<String> regist(@RequestBody ClientUserRegistDTO clientUserRegistDTO) {
         clientUserService.add(clientUserRegistDTO);
         return Result.success();
+    }
+
+    /**
+     * C端用户登录
+     *
+     * @param clientUserLoginDTO C端用户登录DTO
+     */
+    @PostMapping("/login")
+    public Result<ClientUserLoginVO> login(@RequestBody ClientUserLoginDTO clientUserLoginDTO) {
+        ClientUserLoginVO clientUserLoginVO = clientUserService.login(clientUserLoginDTO);
+        return Result.success(clientUserLoginVO);
     }
 }
