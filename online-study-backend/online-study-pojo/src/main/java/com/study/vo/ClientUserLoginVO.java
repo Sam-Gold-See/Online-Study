04799f4045c8210f586988473f8cf48c0e78fe05
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 13:03:06 +0800

    Pojo层实现C端用户登录VO对象

---
+++ online-study-backend/online-study-pojo/src/main/java/com/study/vo/ClientUserLoginVO.java
@@ -0,0 +1,27 @@
+package com.study.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientUserLoginVO implements Serializable {
+
+    // 时间戳唯一id
+    private Long id;
+
+    // 用户名
+    private String name;
+
+    // 用户邮箱号
+    private String email;
+
+    // C端Jwt令牌
+    private String authentication;
+}
