af5600cafc0b071c2457d0d1e9b304aadc2205b5
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 13:14:38 +0800

    更新账号处于封禁状态的信息提示常量类

--- online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
@@ -8,4 +8,5 @@ public class MessageConstant {
     public static final String EMAIL_REPEAT = "请求过于频繁，请稍后再试";
     public static final String VERIFICATION_CODE_ERROR = "验证码错误";
     public static final String ACCOUNT_NOT_FOUND = "账号不存在";
+    public static final String ACCOUNT_LOCKED = "账号处于封禁状态";
 }
