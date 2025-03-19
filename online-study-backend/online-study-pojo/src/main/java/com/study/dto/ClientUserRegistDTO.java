fee743b92a7f2cbc53cc5cdecade548f4393fee8
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 12:58:47 +0800

    Pojo层更新C端用户注册DTO类

---
+++ online-study-backend/online-study-pojo/src/main/java/com/study/dto/ClientUserRegistDTO.java
@@ -0,0 +1,30 @@
+package com.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientUserRegistDTO implements Serializable {
+
+    // 用户名
+    private String name;
+
+    // 用户邮箱号
+    private String email;
+
+    // 用户登录密码
+    private String password;
+
+    // 用户性别（M：男，女：F）
+    private Character gender;
+
+    // 验证码
+    private String verificationCode;
+}
