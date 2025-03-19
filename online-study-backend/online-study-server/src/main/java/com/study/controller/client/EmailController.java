25f9a95746572a89b3bfb8558e9d510b2d74eedb
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 12:24:51 +0800

    Controller层实现获取 请求验证码 请求的功能

---
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/EmailController.java
@@ -0,0 +1,29 @@
+package com.study.controller.client;
+
+import com.study.result.Result;
+import com.study.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/client/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    /**
+     * 请求验证码
+     *
+     * @param email 目标邮箱
+     */
+    @GetMapping("/sendCode")
+    public Result<String> sendCode(String email) {
+        emailService.sendCode(email);
+        return Result.success();
+    }
+}
