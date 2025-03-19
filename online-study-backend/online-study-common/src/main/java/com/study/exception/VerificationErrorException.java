394b03b6125b7f84673174ca93d910d275284da2
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 12:56:27 +0800

    实现验证错误相关业务异常类

---
+++ online-study-backend/online-study-common/src/main/java/com/study/exception/VerificationErrorException.java
@@ -0,0 +1,7 @@
+package com.study.exception;
+
+public class VerificationErrorException extends BusinessException {
+    public VerificationErrorException(String message) {
+        super(message);
+    }
+}
