22fba4b56a5e0743c8c67f5b668c5f9741614fed
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 12:55:52 +0800

    更新验证码错误的信息提示常量类

--- online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
@@ -6,4 +6,5 @@ package com.study.constant;
 public class MessageConstant {
     public static final String SEND_EMAIL_FAIL = "发送邮件失败";
     public static final String EMAIL_REPEAT = "请求过于频繁，请稍后再试";
+    public static final String VERIFICATION_CODE_ERROR = "验证码错误";
 }
