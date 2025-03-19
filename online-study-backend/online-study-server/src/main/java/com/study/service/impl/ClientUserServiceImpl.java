c27083b6b5ca68f7c163c22da3ebaf11a70f8507
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 13:38:19 +0800

    Service层更新 C端用户登录 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/ClientUserServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/ClientUserServiceImpl.java
@@ -114,6 +114,7 @@ public class ClientUserServiceImpl implements ClientUserService {
                 .id(clientUserDB.getId())
                 .name(clientUserDB.getName())
                 .email(email)
+                .avatar(clientUserDB.getAvatar())
                 .authentication(authentication)
                 .build();
     }
