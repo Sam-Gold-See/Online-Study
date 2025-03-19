229c3fcc6ca3c52bc0edc7aaf4d64e4563de429f
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 11:00:55 +0800

    实现邮件相关业务异常类以及更新发送邮件失败的信息提示常量类

--- online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
@@ -4,4 +4,5 @@ package com.study.constant;
  * 信息提示常量类
  */
 public class MessageConstant {
+    public static final String SEND_EMAIL_FAIL = "发送邮件失败";
 }
---
+++ online-study-backend/online-study-common/src/main/java/com/study/exception/EmailException.java
@@ -0,0 +1,7 @@
+package com.study.exception;
+
+public class EmailException extends BusinessException {
+    public EmailException(String message) {
+        super(message);
+    }
+}
