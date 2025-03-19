547777239f58636e0c9f4b3cb498f0a21d834341
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 12:58:53 +0800

    Controller层实现获取 C端用户注册 请求的功能

---
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/UserController.java
@@ -0,0 +1,31 @@
+package com.study.controller.client;
+
+import com.study.dto.ClientUserRegistDTO;
+import com.study.result.Result;
+import com.study.service.ClientUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("ClientUserController")
+@Slf4j
+@RequestMapping("/client/user")
+public class UserController {
+
+    @Autowired
+    private ClientUserService clientUserService;
+
+    /**
+     * C端用户注册
+     *
+     * @param clientUserRegistDTO C端用户注册DTO
+     */
+    @PostMapping("/regist")
+    public Result<String> regist(@RequestBody ClientUserRegistDTO clientUserRegistDTO) {
+        clientUserService.add(clientUserRegistDTO);
+        return Result.success();
+    }
+}
