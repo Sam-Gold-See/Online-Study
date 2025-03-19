2c4b4db716f3672b8dd6b6456b47c3efdfec0a9c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-19 13:11:40 +0800

    实现账户相关业务异常类以及更新账号不存在的信息提示常量类

--- online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
@@ -7,4 +7,5 @@ public class MessageConstant {
     public static final String SEND_EMAIL_FAIL = "发送邮件失败";
     public static final String EMAIL_REPEAT = "请求过于频繁，请稍后再试";
     public static final String VERIFICATION_CODE_ERROR = "验证码错误";
+    public static final String ACCOUNT_NOT_FOUND = "账号不存在";
 }
