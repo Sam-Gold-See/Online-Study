b59d5f9e3d2c8be129bac5abbe25db29a0e78ea4
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-20 14:09:05 +0800

    Controller层实现获取 查询C端用户信息 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/admin/UserManagementController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/admin/UserManagementController.java
@@ -3,12 +3,10 @@ package com.study.controller.admin;
 import com.study.dto.ClientUserDTO;
 import com.study.result.Result;
 import com.study.service.ClientUserService;
+import com.study.vo.ClientUserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/userManagement")
@@ -28,5 +26,17 @@ public class UserManagementController {
         clientUserService.editStatus(clientUserDTO);
         log.info("B端设置C端用户(id：{})的登录状态为：{}", clientUserDTO.getId(), clientUserDTO.getStatus());
         return Result.success();
+    }
+
+    /**
+     * 查询C端用户信息
+     *
+     * @param id C端用户id
+     */
+    @GetMapping("/getInfo")
+    public Result<ClientUserVO> getInfo(Long id) {
+        ClientUserVO clientUserVO = clientUserService.getInfo(id);
+        log.info("B端查询C端用户：{}", clientUserVO);
+        return Result.success(clientUserVO);
     }
 }
