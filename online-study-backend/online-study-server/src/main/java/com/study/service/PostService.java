f6c539f0a366ef769e9d885351951ff76e70bc71
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-20 16:15:24 +0800

    Service层实现 新增帖子 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/PostService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/PostService.java
@@ -1,4 +1,13 @@
 package com.study.service;
 
+import com.study.dto.PostDTO;
+
 public interface PostService {
+
+    /**
+     * 新增帖子
+     *
+     * @param postDTO 帖子DTO对象
+     */
+    void add(PostDTO postDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/PostServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/PostServiceImpl.java
@@ -1,13 +1,33 @@
 package com.study.service.impl;
 
+import com.study.context.BaseContext;
+import com.study.dto.PostDTO;
+import com.study.entity.Post;
 import com.study.mapper.PostMapper;
 import com.study.service.PostService;
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService {
 
-    @Mapper
+    @Autowired
     private PostMapper postMapper;
+
+    /**
+     * 新增帖子
+     *
+     * @param postDTO 帖子DTO对象
+     */
+    @Override
+    public void add(PostDTO postDTO) {
+        Post post = new Post();
+        BeanUtils.copyProperties(postDTO, post);
+
+        Long userId = BaseContext.getCurrentId();
+        post.setUserId(userId);
+
+        postMapper.insert(post);
+    }
 }
