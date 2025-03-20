102242a5635b4b25f9a3461c5af4295a9c6e5542
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-20 16:49:18 +0800

    Mapper层实现 根据id获取帖子信息 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/PostMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/PostMapper.java
@@ -6,6 +6,7 @@ import com.study.enumeration.OperationType;
 import com.study.enumeration.TerminalType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PostMapper {
@@ -19,4 +20,12 @@ public interface PostMapper {
     @Insert("INSERT INTO post (user_id, title, content, create_time, update_time, category_id) " +
             "VALUES (#{userId}, #{title}, #{content}, #{createTime}, #{updateTime}, #{categoryId})")
     void insert(Post post);
+
+    /**
+     * 通过id获取帖子信息
+     *
+     * @param id 帖子id
+     */
+    @Select("SELECT * FROM post WHERE id = #{id}")
+    Post getById(Long id);
 }
