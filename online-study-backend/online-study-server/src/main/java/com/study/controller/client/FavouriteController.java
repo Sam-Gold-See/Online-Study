6a5fa8e40b2c14870de096551c2fbb491f69b699
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 19:39:54 +0800

    Controller层实现获取 新增收藏 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/FavouriteController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/FavouriteController.java
@@ -1,8 +1,13 @@
 package com.study.controller.client;
 
+import com.study.context.BaseContext;
+import com.study.dto.FavouriteDTO;
+import com.study.result.Result;
 import com.study.service.FavouriteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,15 @@ public class FavouriteController {
     @Autowired
     private FavouriteService favouriteService;
 
-
+    /**
+     * 新增收藏
+     *
+     * @param favouriteDTO 收藏DTO对象
+     */
+    @PostMapping("/add")
+    public Result<String> add(@RequestBody FavouriteDTO favouriteDTO) {
+        favouriteService.add(favouriteDTO);
+        log.info("C端用户(id:{})新增收藏:{}", BaseContext.getCurrentId(), favouriteDTO);
+        return Result.success();
+    }
 }
