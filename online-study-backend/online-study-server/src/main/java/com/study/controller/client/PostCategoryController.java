5981498a8e982cff1cc1d17c90c905454c246f92
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 13:05:39 +0800

    Controller层实现获取 查询全部帖子种类 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/PostCategoryController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/PostCategoryController.java
@@ -1,10 +1,16 @@
 package com.study.controller.client;
 
+import com.study.context.BaseContext;
+import com.study.entity.PostCategory;
+import com.study.result.Result;
 import com.study.service.PostCategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/client/postCategory")
@@ -13,4 +19,14 @@ public class PostCategoryController {
 
     @Autowired
     private PostCategoryService postCategoryService;
+
+    /**
+     * 获取全部帖子种类
+     */
+    @GetMapping
+    public Result<List<PostCategory>> getList() {
+        List<PostCategory> list = postCategoryService.getList();
+        log.info("C端用户(id:{})正在获取全部帖子种类", BaseContext.getCurrentId());
+        return Result.success(list);
+    }
 }
