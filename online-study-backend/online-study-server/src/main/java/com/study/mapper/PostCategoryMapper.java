92851ca925890a0cb8ea758b59da072c0c53e9d8
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 13:05:08 +0800

    Mapper层实现 获取全部帖子种类 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/PostCategoryMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/PostCategoryMapper.java
@@ -1,7 +1,17 @@
 package com.study.mapper;
 
+import com.study.entity.PostCategory;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PostCategoryMapper {
+
+    /**
+     * 获取全部帖子种类
+     */
+    @Select("SELECT * FROM post_category")
+    List<PostCategory> getList();
 }
