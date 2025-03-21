7def55df708c4a8bc487dc0aece960e483889fbe
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 17:02:29 +0800

    Pojo层实现收藏DTO对象

---
+++ online-study-backend/online-study-pojo/src/main/java/com/study/dto/FavouriteDTO.java
@@ -0,0 +1,24 @@
+package com.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FavouriteDTO implements Serializable {
+
+    // 收藏id
+    private Integer id;
+
+    // 用户id
+    private Long userId;
+
+    // 帖子id
+    private Integer postId;
+}
