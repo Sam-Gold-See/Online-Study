02be5e171315a53557b8dee15699ad137a5ff92c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 16:51:29 +0800

    Pojo层实现收藏实体类对象

---
+++ online-study-backend/online-study-pojo/src/main/java/com/study/entity/Favourite.java
@@ -0,0 +1,35 @@
+package com.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Favourite implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // 收藏id
+    private Integer id;
+
+    // 用户id
+    private Long userId;
+
+    // 帖子id
+    private Integer postId;
+
+    // 创建时间
+    private LocalDateTime createTime;
+
+    // 更新时间
+    private LocalDateTime updateTime;
+}
