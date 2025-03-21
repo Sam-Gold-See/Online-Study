1205ec1cf9e0a17bb18f755559c0bc02d77e1d90
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 10:58:14 +0800

    优化相关代码注释

--- online-study-backend/online-study-common/src/main/java/com/study/constant/AccountConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/AccountConstant.java
@@ -8,7 +8,7 @@ public class AccountConstant {
     // Redis存储验证码键常量
     public static final String REDIS_KEY = "code:list:";
 
-    // 验证码有效时长
+    // 验证码有效时长（分钟）
     public static final Integer VERIFICATION_CODE_TTL = 5;
 
     // 验证码字符数组
