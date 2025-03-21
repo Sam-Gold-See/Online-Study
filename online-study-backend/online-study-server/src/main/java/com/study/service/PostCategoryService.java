cfbc8f5afdcfae07115654ccec69c25a7a1ca122
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 13:05:18 +0800

    Service层实现 获取全部帖子种类 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/PostCategoryService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/PostCategoryService.java
@@ -1,4 +1,13 @@
 package com.study.service;
 
+import com.study.entity.PostCategory;
+
+import java.util.List;
+
 public interface PostCategoryService {
+
+    /**
+     * 获取全部帖子种类
+     */
+    List<PostCategory> getList();
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/PostCategoryServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/PostCategoryServiceImpl.java
@@ -1,13 +1,24 @@
 package com.study.service.impl;
 
+import com.study.entity.PostCategory;
 import com.study.mapper.PostCategoryMapper;
 import com.study.service.PostCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostCategoryServiceImpl implements PostCategoryService {
 
     @Autowired
     private PostCategoryMapper postCategoryMapper;
+
+    /**
+     * 获取全部帖子种类
+     */
+    @Override
+    public List<PostCategory> getList() {
+        return postCategoryMapper.getList();
+    }
 }
