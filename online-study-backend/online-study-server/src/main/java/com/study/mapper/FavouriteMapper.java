84358d9544daa11373e6cce063608d39721cd6e8
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 19:39:35 +0800

    Mapper层实现 新增收藏 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/FavouriteMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/FavouriteMapper.java
@@ -1,7 +1,22 @@
 package com.study.mapper;
 
+import com.study.annotation.AutoFill;
+import com.study.entity.Favourite;
+import com.study.enumeration.OperationType;
+import com.study.enumeration.TerminalType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FavouriteMapper {
+
+    /**
+     * 新增收藏
+     *
+     * @param favourite 收藏实体类对象
+     */
+    @AutoFill(operation = OperationType.INSERT, terminal = TerminalType.CLIENT)
+    @Insert("INSERT INTO favourite (user_id, post_id, create_time, update_time) " +
+            "VALUES (#{userId}, #{postId}, #{createTime}, #{updateTime})")
+    void insert(Favourite favourite);
 }
