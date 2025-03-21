b6e42430a8ebe6f528e3cfe5f0c151f639b86d09
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 19:52:54 +0800

    Controller层实现获取 删除收藏 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/FavouriteController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/FavouriteController.java
@@ -6,10 +6,7 @@ import com.study.result.Result;
 import com.study.service.FavouriteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/client/favourite")
@@ -28,6 +25,18 @@ public class FavouriteController {
     public Result<String> add(@RequestBody FavouriteDTO favouriteDTO) {
         favouriteService.add(favouriteDTO);
         log.info("C端用户(id:{})新增收藏:{}", BaseContext.getCurrentId(), favouriteDTO);
+        return Result.success();
+    }
+
+    /**
+     * 删除收藏
+     *
+     * @param favouriteDTO 收藏DTO对象
+     */
+    @DeleteMapping("/delete")
+    public Result<String> delete(@RequestBody FavouriteDTO favouriteDTO) {
+        favouriteService.delete(favouriteDTO);
+        log.info("C端用户(id:{})删除收藏：{}", BaseContext.getCurrentId(), favouriteDTO);
         return Result.success();
     }
 }
