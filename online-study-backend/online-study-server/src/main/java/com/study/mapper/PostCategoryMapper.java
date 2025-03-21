953f7843d99b211e81894c4c5c8e341b21a9695e
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 13:34:03 +0800

    Mapper层实现 新增帖子种类 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/PostCategoryMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/PostCategoryMapper.java
@@ -14,4 +14,11 @@ public interface PostCategoryMapper {
      */
     @Select("SELECT * FROM post_category")
     List<PostCategory> getList();
+
+    /**
+     * 新增帖子种类
+     *
+     * @param postCategory 帖子种类实体类对象
+     */
+    Integer add(PostCategory postCategory);
 }
