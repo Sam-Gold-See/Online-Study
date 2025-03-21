7876873beac0073ac0840550c6218e862df34d46
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 13:34:45 +0800

    Controller层实现获取 新增帖子种类 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/admin/PostCategoryManagementController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/admin/PostCategoryManagementController.java
@@ -27,4 +27,16 @@ public class PostCategoryManagementController {
         log.info("B端用户(id:{})查询全部帖子种类", BaseContext.getCurrentId());
         return Result.success(list);
     }
+
+    /**
+     * 新增帖子种类
+     *
+     * @param name 帖子种类名称
+     */
+    @PutMapping("/add")
+    public Result<String> add(String name) {
+        PostCategory postCategory = postCategoryService.add(name);
+        log.info("B端用户(id:{})新增帖子种类:{}", BaseContext.getCurrentId(), postCategory);
+        return Result.success();
+    }
 }
