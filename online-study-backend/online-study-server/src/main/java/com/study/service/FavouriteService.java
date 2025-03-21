81e833588c9e8743eefa83ed44cb486accc8bff3
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-21 19:39:43 +0800

    Service层实现 新增收藏 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/FavouriteService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/FavouriteService.java
@@ -1,4 +1,13 @@
 package com.study.service;
 
+import com.study.dto.FavouriteDTO;
+
 public interface FavouriteService {
+
+    /**
+     * 新增收藏
+     *
+     * @param favouriteDTO 收藏DTO对象
+     */
+    void add(FavouriteDTO favouriteDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/FavouriteServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/FavouriteServiceImpl.java
@@ -1,7 +1,11 @@
 package com.study.service.impl;
 
+import com.study.context.BaseContext;
+import com.study.dto.FavouriteDTO;
+import com.study.entity.Favourite;
 import com.study.mapper.FavouriteMapper;
 import com.study.service.FavouriteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +14,18 @@ public class FavouriteServiceImpl implements FavouriteService {
 
     @Autowired
     private FavouriteMapper favouriteMapper;
+
+    /**
+     * 新增收藏
+     *
+     * @param favouriteDTO 收藏DTO对象
+     */
+    @Override
+    public void add(FavouriteDTO favouriteDTO) {
+        Favourite favourite = new Favourite();
+        BeanUtils.copyProperties(favouriteDTO, favourite);
+        favourite.setUserId(BaseContext.getCurrentId());
+
+        favouriteMapper.insert(favourite);
+    }
 }
