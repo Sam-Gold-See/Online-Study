3145d1f2b0a903f049db85c3e975d73c8312ffed
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 13:37:05 +0800

    Service层实现 新增点赞 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/LikeService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/LikeService.java
@@ -1,4 +1,13 @@
 package com.study.service;
 
+import com.study.dto.LikeDTO;
+
 public interface LikeService {
+
+    /**
+     * 新增点赞
+     *
+     * @param likeDTO 点赞DTO对象
+     */
+    void add(LikeDTO likeDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/LikeServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/LikeServiceImpl.java
@@ -1,7 +1,11 @@
 package com.study.service.impl;
 
+import com.study.context.BaseContext;
+import com.study.dto.LikeDTO;
+import com.study.entity.Like;
 import com.study.mapper.LikeMapper;
 import com.study.service.LikeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -14,4 +18,18 @@ public class LikeServiceImpl implements LikeService {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 新增点赞
+     *
+     * @param likeDTO 点赞DTO对象
+     */
+    @Override
+    public void add(LikeDTO likeDTO) {
+        Like like = new Like();
+        BeanUtils.copyProperties(likeDTO, like);
+        like.setUserId(BaseContext.getCurrentId());
+
+        likeMapper.insert(like);
+    }
 }
