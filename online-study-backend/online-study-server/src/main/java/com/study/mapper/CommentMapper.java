a0780d852ed13a7e40bab9e39735a25641a91482
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 18:56:08 +0800

    Mapper层实现 根据id查询评论 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/CommentMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/CommentMapper.java
@@ -6,6 +6,7 @@ import com.study.enumeration.OperationType;
 import com.study.enumeration.TerminalType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface CommentMapper {
@@ -19,4 +20,12 @@ public interface CommentMapper {
     @Insert("INSERT INTO comment (post_id, user_id, parent_id, content, create_time, update_time) " +
             "VALUES (#{postId}, #{userId}, #{parentId}, #{content}, #{createTime}, #{updateTime})")
     void insert(Comment comment);
+
+    /**
+     * 根据id查询评论
+     *
+     * @param id 评论id
+     */
+    @Select("SELECT * FROM comment WHERE id = #{id}")
+    Comment getById(Long id);
 }
