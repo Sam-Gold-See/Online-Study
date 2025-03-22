62d03ed4dbfac6fb8597b77752c8fd06efb3c0cf
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 18:30:51 +0800

    Controller层实现获取 新增评论 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/CommentController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/CommentController.java
@@ -1,8 +1,13 @@
 package com.study.controller.client;
 
+import com.study.context.BaseContext;
+import com.study.dto.CommentDTO;
+import com.study.result.Result;
 import com.study.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,16 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    /**
+     * 新增评论
+     *
+     * @param commentDTO 评论DTO对象
+     */
+    @PutMapping("/add")
+    public Result<String> add(@RequestBody CommentDTO commentDTO) {
+        commentService.add(commentDTO);
+        log.info("C端用户(id:{})新增了评论:{}", BaseContext.getCurrentId(), commentDTO);
+        return Result.success();
+    }
 }
