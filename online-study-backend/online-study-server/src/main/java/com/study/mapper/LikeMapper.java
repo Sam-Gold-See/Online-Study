155acb4ecd1456a5d7b50c9cd0a4ef60d7a9429c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 13:36:56 +0800

    Mapper层实现 新增点赞 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/LikeMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/LikeMapper.java
@@ -1,7 +1,22 @@
 package com.study.mapper;
 
+import com.study.annotation.AutoFill;
+import com.study.entity.Like;
+import com.study.enumeration.OperationType;
+import com.study.enumeration.TerminalType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface LikeMapper {
+
+    /**
+     * 新增点赞
+     *
+     * @param like 点赞实体类对象
+     */
+    @AutoFill(operation = OperationType.INSERT, terminal = TerminalType.CLIENT)
+    @Insert("INSERT INTO `like` (user_id, post_id, comment_id, create_time) " +
+            "VALUES (#{userId}, #{postId}, #{commentId}, #{createTime})")
+    void insert(Like like);
 }
