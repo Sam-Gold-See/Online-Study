789addbf5f978b1b920ef7454914e7ebde9a18f8
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 19:03:03 +0800

    Controller层实现获取 删除评论 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/CommentController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/CommentController.java
@@ -6,10 +6,7 @@ import com.study.result.Result;
 import com.study.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/client/comment")
@@ -27,7 +24,19 @@ public class CommentController {
     @PutMapping("/add")
     public Result<String> add(@RequestBody CommentDTO commentDTO) {
         commentService.add(commentDTO);
-        log.info("C端用户(id:{})新增了评论:{}", BaseContext.getCurrentId(), commentDTO);
+        log.info("C端用户(id:{})新增评论:{}", BaseContext.getCurrentId(), commentDTO);
+        return Result.success();
+    }
+
+    /**
+     * 删除评论
+     *
+     * @param commentDTO 评论DTO对象
+     */
+    @DeleteMapping("/delete")
+    public Result<String> delete(@RequestBody CommentDTO commentDTO) {
+        commentService.delete(commentDTO);
+        log.info("C端用户(id:{})删除评论：{}", BaseContext.getCurrentId(), commentDTO);
         return Result.success();
     }
 }
