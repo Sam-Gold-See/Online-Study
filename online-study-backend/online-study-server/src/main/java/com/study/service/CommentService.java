ffbffd60a6f5c8d0361eed0c383feba03390e84b
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 18:30:37 +0800

    Service层实现 新增评论 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/CommentService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/CommentService.java
@@ -1,4 +1,13 @@
 package com.study.service;
 
+import com.study.dto.CommentDTO;
+
 public interface CommentService {
+
+    /**
+     * 新增评论
+     *
+     * @param commentDTO 评论DTO对象
+     */
+    void add(CommentDTO commentDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/CommentServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/CommentServiceImpl.java
@@ -1,7 +1,11 @@
 package com.study.service.impl;
 
+import com.study.context.BaseContext;
+import com.study.dto.CommentDTO;
+import com.study.entity.Comment;
 import com.study.mapper.CommentMapper;
 import com.study.service.CommentService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +14,18 @@ public class CommentServiceImpl implements CommentService {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    /**
+     * 新增评论
+     *
+     * @param commentDTO 评论DTO对象
+     */
+    @Override
+    public void add(CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        BeanUtils.copyProperties(commentDTO, comment);
+        comment.setUserId(BaseContext.getCurrentId());
+
+        commentMapper.insert(comment);
+    }
 }
