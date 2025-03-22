a6763d7d6ae70af5ae87996543568ae119bd7779
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 13:37:19 +0800

    Controller层实现获取 新增点赞 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/client/LikeController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/client/LikeController.java
@@ -1,8 +1,13 @@
 package com.study.controller.client;
 
+import com.study.context.BaseContext;
+import com.study.dto.LikeDTO;
+import com.study.result.Result;
 import com.study.service.LikeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,16 @@ public class LikeController {
 
     @Autowired
     private LikeService likeService;
+
+    /**
+     * 新增点赞
+     *
+     * @param likeDTO 点赞DTO对象
+     */
+    @PutMapping("/add")
+    public Result<String> add(@RequestBody LikeDTO likeDTO) {
+        likeService.add(likeDTO);
+        log.info("C端用户(id:{})新增收藏:{}", BaseContext.getCurrentId(), likeDTO);
+        return Result.success();
+    }
 }
