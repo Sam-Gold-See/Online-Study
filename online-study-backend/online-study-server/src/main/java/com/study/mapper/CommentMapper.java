e4898381437b530544d87e045226f17bb59dae24
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-22 18:30:27 +0800

    Mapper层实现 新增评论 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/CommentMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/CommentMapper.java
@@ -1,7 +1,22 @@
 package com.study.mapper;
 
+import com.study.annotation.AutoFill;
+import com.study.entity.Comment;
+import com.study.enumeration.OperationType;
+import com.study.enumeration.TerminalType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CommentMapper {
+
+    /**
+     * 新增评论
+     *
+     * @param comment 评论实体类对象
+     */
+    @AutoFill(operation = OperationType.INSERT, terminal = TerminalType.CLIENT)
+    @Insert("INSERT INTO comment (post_id, user_id, parent_id, content, create_time, update_time) " +
+            "VALUES (#{postId}, #{userId}, #{parentId}, #{content}, #{createTime}, #{updateTime})")
+    void insert(Comment comment);
 }
