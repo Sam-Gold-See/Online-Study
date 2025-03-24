10b24e8e300b8cf97b4aaf1f2d1f9071767cd5d0
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-24 15:31:30 +0800

    Controller层获取 分页查询评论 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/admin/CommentManagementController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/admin/CommentManagementController.java
@@ -1,12 +1,15 @@
 package com.study.controller.admin;
 
 import com.study.context.BaseContext;
+import com.study.dto.CommentPageQueryDTO;
 import com.study.entity.Comment;
+import com.study.result.PageResult;
 import com.study.result.Result;
 import com.study.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,17 @@ public class CommentManagementController {
         Comment comment = commentService.get(id);
         log.info("B端用户(id:{})查询评论详情(id:{})", BaseContext.getCurrentId(), id);
         return Result.success(comment);
+    }
+
+    /**
+     * 分页查询评论
+     *
+     * @param commentPageQueryDTO 评论分页查询DTO
+     */
+    @GetMapping("/query")
+    public Result<PageResult<Comment>> query(@RequestBody CommentPageQueryDTO commentPageQueryDTO) {
+        PageResult<Comment> pageResult = commentService.query(commentPageQueryDTO);
+        log.info("B端用户(id:{})分页查询评论", BaseContext.getCurrentId());
+        return Result.success(pageResult);
     }
 }
