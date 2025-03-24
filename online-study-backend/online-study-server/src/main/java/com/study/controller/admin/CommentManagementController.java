3849f0b70548465e315c5574d50e64899fcd3388
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-24 15:20:17 +0800

    Controller层实现获取 查询评论详情 请求的功能

--- online-study-backend/online-study-server/src/main/java/com/study/controller/admin/CommentManagementController.java
+++ online-study-backend/online-study-server/src/main/java/com/study/controller/admin/CommentManagementController.java
@@ -1,8 +1,12 @@
 package com.study.controller.admin;
 
+import com.study.context.BaseContext;
+import com.study.entity.Comment;
+import com.study.result.Result;
 import com.study.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,16 @@ public class CommentManagementController {
 
     @Autowired
     private CommentService commentService;
+
+    /**
+     * 查询评论详情
+     *
+     * @param id 评论id
+     */
+    @GetMapping()
+    public Result<Comment> get(Long id) {
+        Comment comment = commentService.get(id);
+        log.info("B端用户(id:{})查询评论详情(id:{})", BaseContext.getCurrentId(), id);
+        return Result.success(comment);
+    }
 }
