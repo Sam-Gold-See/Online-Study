8ce49262065f00671aff3652d33694a6a9c84b1c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-07 21:30:28 +0800

    更新信息提示常量类 账号已存在

--- online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/MessageConstant.java
@@ -7,6 +7,7 @@ public class MessageConstant {
     public static final String SEND_EMAIL_FAIL = "发送邮件失败";
     public static final String EMAIL_REPEAT = "请求过于频繁，请稍后再试";
     public static final String VERIFICATION_CODE_ERROR = "验证码错误";
+    public static final String ACCOUNT_ALREADY_EXIST = "账号已存在";
     public static final String ACCOUNT_NOT_FOUND = "账号不存在";
     public static final String ACCOUNT_LOCKED = "账号处于封禁状态";
     public static final String PASSWORD_ERROR = "密码错误";
