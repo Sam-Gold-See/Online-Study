32dd15ce7e30f6cbd1991737fcdd2fd9cf6d54c6
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-08 18:04:15 +0800

    Service层实现 校对验证码 的业务需求

--- online-study-backend/online-study-server/src/main/java/com/study/service/EmailService.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.study.service;
 
+import com.study.dto.ClientUserDTO;
+
 public interface EmailService {
 
     /**
@@ -8,4 +10,11 @@ public interface EmailService {
      * @param email 目标邮箱
      */
     void sendCode(String email);
+
+    /**
+     * 验证验证码
+     *
+     * @param clientUserDTO C端用户DTO
+     */
+    void checkCode(ClientUserDTO clientUserDTO);
 }
--- online-study-backend/online-study-server/src/main/java/com/study/service/impl/EmailServiceImpl.java
+++ online-study-backend/online-study-server/src/main/java/com/study/service/impl/EmailServiceImpl.java
@@ -2,14 +2,18 @@ package com.study.service.impl;
 
 import com.study.constant.AccountConstant;
 import com.study.constant.MessageConstant;
+import com.study.dto.ClientUserDTO;
 import com.study.exception.EmailException;
+import com.study.exception.VerificationException;
 import com.study.service.EmailService;
+import com.study.utils.CodeUtils;
 import com.study.utils.EmailUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -33,5 +37,26 @@ public class EmailServiceImpl implements EmailService {
         EmailUtils.sendVerificationCode(email, verificationCode);
 
         stringRedisTemplate.opsForValue().set(AccountConstant.REDIS_KEY + email, verificationCode, AccountConstant.VERIFICATION_CODE_TTL, TimeUnit.MINUTES);
+    }
+
+    /**
+     * 验证验证码
+     *
+     * @param clientUserDTO C端用户DTO
+     */
+    @Override
+    public void checkCode(ClientUserDTO clientUserDTO) {
+        String verificationCode = clientUserDTO.getVerificationCode();
+        String email = clientUserDTO.getEmail();
+        verificationCode = CodeUtils.upperLetters(verificationCode);
+        String verificationCodeRedis = stringRedisTemplate.opsForValue().get(AccountConstant.REDIS_KEY + email);
+
+        // 验证码比对
+        if (verificationCodeRedis == null || !Objects.equals(verificationCodeRedis, verificationCode)) {
+            throw new VerificationException(MessageConstant.VERIFICATION_CODE_ERROR);
+        }
+
+        // 验证码正确，设置 Redis 中的验证码状态为true
+        stringRedisTemplate.opsForValue().set(AccountConstant.REDIS_KEY + email, AccountConstant.PERMISSION.toString(), AccountConstant.VERIFICATION_CODE_TTL, TimeUnit.MINUTES);
     }
 }
