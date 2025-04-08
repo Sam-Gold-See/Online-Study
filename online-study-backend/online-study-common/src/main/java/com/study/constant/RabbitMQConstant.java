84d83e38268059136bf2ab925e2418cea30d92b1
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-09 04:25:24 +0800

    实现RabbitMQ消息队列常量类

---
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/RabbitMQConstant.java
@@ -0,0 +1,16 @@
+package com.study.constant;
+
+/**
+ * RabbitMQ消息队列常量类
+ */
+public class RabbitMQConstant {
+
+    // 通知交换机名
+    public static final String NOTIFICATION_EXCHANGE = "notification.exchange";
+
+    // 通知队列名
+    public static final String NOTIFICATION_QUEUE = "notification.queue";
+
+    // 通知路由键
+    public static final String NOTIFICATION_ROUTING_KEY = "notification.route";
+}
