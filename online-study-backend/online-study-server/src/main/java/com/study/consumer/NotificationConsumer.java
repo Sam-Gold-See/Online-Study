7c9cebe926ff62191995e813220f1ce592d405b1
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-09 17:18:05 +0800

    Consumer层实现 接收通知并存储 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/consumer/NotificationConsumer.java
+++ online-study-backend/online-study-server/src/main/java/com/study/consumer/NotificationConsumer.java
@@ -1,16 +1,35 @@
 package com.study.consumer;
 
 import com.study.constant.RabbitMQConstant;
+import com.study.dto.NotificationDTO;
+import com.study.entity.Notification;
+import com.study.mapper.NotificationMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RabbitListener(queues = RabbitMQConstant.NOTIFICATION_QUEUE)
 public class NotificationConsumer {
 
-    @RabbitListener(queues = RabbitMQConstant.NOTIFICATION_QUEUE)
-    public void receiveNotification(String message) {
-        log.info("receive consumer : {}", message);
+    @Autowired
+    private NotificationMapper notificationMapper;
+
+    /**
+     * 接收到通知DTO对象存入数据库
+     *
+     * @param notificationDTO 通知DTO对象
+     */
+    @RabbitHandler
+    public void receiveNotification(NotificationDTO notificationDTO) {
+        Notification notification = new Notification();
+
+        BeanUtils.copyProperties(notificationDTO, notification);
+
+        notificationMapper.insert(notification);
     }
 }
