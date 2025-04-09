6debac83401dde3606094cdaa10bbdde4edf8539
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-09 15:31:35 +0800

    Producer层实现 发送通知DTO到通知队列 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/producer/NotificationProducer.java
+++ online-study-backend/online-study-server/src/main/java/com/study/producer/NotificationProducer.java
@@ -1,5 +1,7 @@
 package com.study.producer;
 
+import com.study.constant.RabbitMQConstant;
+import com.study.dto.NotificationDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +15,14 @@ public class NotificationProducer {
     private RabbitTemplate rabbitTemplate;
 
     /**
-     * 发送消息到指定队列
+     * 发送通知DTO对象到通知队列
      *
-     * @param exchange   交换机
-     * @param routingKey 路由键
-     * @param message    消息内容
+     * @param notificationDTO 通知DTO对象
      */
-    public void sendMessage(String exchange, String routingKey, Object message) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    public void sendNotificationDTO(NotificationDTO notificationDTO) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConstant.NOTIFICATION_EXCHANGE,
+                RabbitMQConstant.NOTIFICATION_ROUTING_KEY,
+                notificationDTO);
     }
 }
