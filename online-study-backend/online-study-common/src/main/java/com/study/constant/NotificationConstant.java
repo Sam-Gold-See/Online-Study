37feae96b318c08ad9bc80be2bf86dde3e414948
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-09 17:32:32 +0800

    实现通知常量类

---
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/NotificationConstant.java
@@ -0,0 +1,16 @@
+package com.study.constant;
+
+/**
+ * 通知常量类
+ */
+public class NotificationConstant {
+
+    // 通知类型为点赞
+    public static final Integer LIKE = 1;
+
+    // 通知类型为评论
+    public static final Integer COMMENT = 2;
+
+    // 通知类型为回复
+    public static final Integer REPLY = 3;
+}
