680cf606497ef29e5cab30a7b372c852dda6a47e
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-10 13:27:53 +0800

    Mapper层实现 查询通知 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/mapper/NotificationMapper.java
+++ online-study-backend/online-study-server/src/main/java/com/study/mapper/NotificationMapper.java
@@ -4,8 +4,12 @@ import com.study.annotation.AutoFill;
 import com.study.entity.Notification;
 import com.study.enumeration.OperationType;
 import com.study.enumeration.TerminalType;
+import com.study.vo.NotificationVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface NotificationMapper {
@@ -19,4 +23,12 @@ public interface NotificationMapper {
     @Insert("INSERT INTO notification (from_id, to_id, type, post_id, comment_id, content, create_time, update_time) " +
             "VALUES (#{fromId}, #{toId}, #{type}, #{postId}, #{commentId}, #{content}, #{createTime}, #{updateTime})")
     void insert(Notification notification);
+
+    /**
+     * 查询通知
+     *
+     * @param notification 通知实体类对象
+     */
+    @Select("SELECT * FROM notification WHERE to_id = #{toId} AND status = #{status} ORDER BY create_time DESC")
+    List<NotificationVO> getList(Notification notification);
 }
