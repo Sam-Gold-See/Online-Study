6bfb011d681bcda3af9de227f4ca514a59145f4e
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-10 10:57:59 +0800

    更新通知信息常量类 通知内容

--- online-study-backend/online-study-common/src/main/java/com/study/constant/NotificationConstant.java
+++ online-study-backend/online-study-common/src/main/java/com/study/constant/NotificationConstant.java
@@ -13,4 +13,16 @@ public class NotificationConstant {
 
     // 通知类型为回复
     public static final Integer REPLY = 3;
+
+    // 点赞帖子通知内容
+    public static final String LIKE_POST_NOTIFICATION = "您的帖子收到了一条点赞";
+
+    // 点赞评论通知内容
+    public static final String LIKE_COMMENT_NOTIFICATION = "您的评论收到了一条点赞";
+
+    // 评论通知内容
+    public static final String COMMENT_NOTIFICATION = "您的帖子收到了一条评论";
+
+    // 回复通知内容
+    public static final String REPLY_NOTIFICATION = "您的评论收到了一条回复";
 }
