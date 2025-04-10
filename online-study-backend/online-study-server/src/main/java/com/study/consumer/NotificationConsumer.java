523e4b8a53bcfbb0cabb5f36b1597da5918018d1
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-10 10:59:21 +0800

    Consumer层优化 处理消息通知 的功能

--- online-study-backend/online-study-server/src/main/java/com/study/consumer/NotificationConsumer.java
+++ online-study-backend/online-study-server/src/main/java/com/study/consumer/NotificationConsumer.java
@@ -1,9 +1,12 @@
 package com.study.consumer;
 
+import com.study.constant.NotificationConstant;
 import com.study.constant.RabbitMQConstant;
 import com.study.dto.NotificationDTO;
 import com.study.entity.Notification;
+import com.study.mapper.CommentMapper;
 import com.study.mapper.NotificationMapper;
+import com.study.mapper.PostMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,10 +14,18 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 @Slf4j
 @RabbitListener(queues = RabbitMQConstant.NOTIFICATION_QUEUE)
 public class NotificationConsumer {
+
+    @Autowired
+    private PostMapper postMapper;
+
+    @Autowired
+    private CommentMapper commentMapper;
 
     @Autowired
     private NotificationMapper notificationMapper;
@@ -29,6 +40,27 @@ public class NotificationConsumer {
         Notification notification = new Notification();
 
         BeanUtils.copyProperties(notificationDTO, notification);
+
+
+        // 获取接收通知的用户 ID
+        Long toId = (notification.getCommentId() == null)
+                ? postMapper.getUserId(notification.getPostId())
+                : commentMapper.getUserId(notification.getCommentId());
+
+        // 设置通知内容
+        String content;
+        if (Objects.equals(notification.getType(), NotificationConstant.LIKE)) {
+            content = (notification.getCommentId() == null)
+                    ? NotificationConstant.LIKE_POST_NOTIFICATION
+                    : NotificationConstant.LIKE_COMMENT_NOTIFICATION;
+        } else {
+            content = (notification.getCommentId() == null)
+                    ? NotificationConstant.COMMENT_NOTIFICATION
+                    : NotificationConstant.REPLY_NOTIFICATION;
+        }
+
+        notification.setToId(toId);
+        notification.setContent(content);
 
         notificationMapper.insert(notification);
         log.info("接收通知:{}", notification);
